5e45f8ccc7495ac94efad75c3a30c20510485c61
Bin Tran <sudocode.bin@gmail.com>
2017-08-28 00:19:48 -0700

    changed database for upload

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // SET UP DATABASE SERVER
-    database: 'mongodb://sohonailskerman:sweetkerman@ds163053.mlab.com:63053/heroku_n5qhc22t' || 'mongodb://localhost:27017/soho-nails-app',
+    database: 'mongodb://heroku_n5qhc22t:4abqdgj3ia6qnere4iojhlqlr7@ds163053.mlab.com:63053/heroku_n5qhc22t' || 'mongodb://localhost:27017/soho-nails-app',
     tokenSecret: 'sohosecret'
 }
